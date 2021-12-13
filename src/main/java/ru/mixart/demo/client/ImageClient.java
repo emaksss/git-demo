6cf09c6e69fbd215d0aa30527e0029e7d9598016
Maksim <94253901+emaksss@users.noreply.github.com>
2021-12-13 13:24:42 +0300

    3
    3

--- src/main/java/ru/mixart/demo/client/ImageClient.java
+++
@@ -1,9 +0,0 @@
-package ru.mixart.demo.client;
-
-import org.springframework.http.ResponseEntity;
-
-import java.util.Map;
-
-public interface ImageClient {
-    ResponseEntity<Map> getRandomImage(String apiKey, String tag, String rating);
-}
--- src/main/java/ru/mixart/demo/service/RatesService.java
+++
@@ -1,9 +0,0 @@
-package ru.mixart.demo.service;
-
-import java.util.List;
-
-public interface RatesService {
-    void downloadRates();
-    List<String> getCodes();
-    int compareRates(String charCode);
-}
