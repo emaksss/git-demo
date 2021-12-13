61d89c92f30deb7d7c905a428e2434989b958e12
Maksim <94253901+emaksss@users.noreply.github.com>
2021-12-13 13:27:14 +0300

    6
    6

--- demo/src/main/java/ru/mixart/demo/service/ImageService.java
+++
@@ -1,9 +0,0 @@
-package ru.mixart.demo.service;
-
-import org.springframework.http.ResponseEntity;
-
-import java.util.Map;
-
-public interface ImageService {
-    ResponseEntity<Map> getImage(String tag);
-}
--- demo/src/main/java/ru/mixart/service/ImageService.java
+++
@@ -1,9 +0,0 @@
-package ru.mixart.service;
-
-import org.springframework.http.ResponseEntity;
-
-import java.util.Map;
-
-public interface ImageService {
-    ResponseEntity<Map> getImage(String tag);
-}
